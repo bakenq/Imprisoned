b171a1d647b1946b961b317a3f78c25515f015f0
Bennet <Bennetwiebke@googlemail.com>
2021-12-08 05:20:45 +0100

    Title Screen, Addon Dialogic, NPC Dialog

---
+++ Scenes/menus/MainMenu.gd
@@ -0,0 +1,13 @@
+extends Node
+
+func _ready():
+	$VBoxContainer/VBoxContainer/Start.grab_focus()
+
+func _on_Start_pressed():
+	get_tree().change_scene("res://Scenes/World.tscn")
+
+func _on_Options_pressed():
+	print("Options pressed")
+
+func _on_Exit_pressed():
+	get_tree().quit()
