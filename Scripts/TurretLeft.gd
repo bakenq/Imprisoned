6ccbe2020b974482943c03cbbab62e76f8318a21
Niko <nikolasm@outlook.de>
2022-01-12 11:21:43 +0100

    Fireball can shoot left or right

---
+++ Scripts/TurretLeft.gd
@@ -0,0 +1,23 @@
+extends Node2D
+
+var FIREBALL = preload("res://Scenes/FireballLeft.tscn")
+
+var ready = true
+
+func _ready():
+	pass # Replace with function body.
+	
+func _physics_process(delta):
+	if ready:
+		shoot()
+
+func shoot():
+	var fireball: Node2D = FIREBALL.instance()
+	get_tree().current_scene.add_child(fireball)
+	fireball.global_position = global_position
+	ready = false
+		
+	$Timer.start(2.0)
+
+func _on_Timer_timeout():
+	ready = true
