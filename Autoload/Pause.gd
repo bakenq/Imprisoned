5b7df9d8e63a5a23ce99b61a535cabd529f80eac
LarryLaxxn <pbmayer@gmx.de>
2022-06-02 01:05:44 +0200

    Pause Menü
    Moin
    Liest das hier überhaupt jemand und viel wichtiger schreibt man liest mit scharfen s oder mit normalen s. Naja wie auch immer ich schweife ab. Wie der Titel schon sagt es gibt jetzt ne pause. es gibt auch n weiter button aber mehr noch nicht ich werd mal sehen wie ich die optionen da rein bekomme

---
+++ Autoload/Pause.gd
@@ -0,0 +1,33 @@
+extends CanvasLayer
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	set_visible(false)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+func _input(event):
+	
+	if event.is_action_pressed("ui_cancel"): #esc btn
+		set_visible(!get_tree().paused)
+		get_tree().paused = !get_tree().paused
+
+
+
+func _on_Button_pressed():
+	get_tree().paused = false
+	set_visible(false)
+	
+	
+func set_visible(is_visible):
+	for node in get_children():
+		node.visible = is_visible
