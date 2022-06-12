6368270585ef50e8a32224d4cf6998d13a964712
LarryLaxxn <pbmayer@gmx.de>
2022-06-12 22:34:03 +0200

    Licht u. Sound
    Moin
    ich hab noch ein paar licht effekte hinzugefügt. Außerdem dem herz n sound gegeben.

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -303,6 +303,10 @@ func hitbyfire(area):
 	if (area.is_in_group("Projectile") && hitpoints > 0):
 		$HitFire.pitch_scale = rand_range(0.8, 1.0)
 		$HitFire.play()
+		
+func heartpickup(area): #Hier nochmal wegen der area schauen was stimmt
+	if (area.is_in_group("Heart")):
+		$heartpickup.play()
 
 
 func _on_LevelEnd_area_entered(area):
